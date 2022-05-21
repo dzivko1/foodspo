b821118b7dbfa0b4a25fe94694072f003c3e6332
Dominik Živko <domy.zivko@gmail.com>
2022-05-21 20:11:46 +0200

    Extract dependency declarations to buildSrc

---
+++ buildSrc/src/main/java/altline/foodspo/buildsrc/Dependencies.kt
@@ -0,0 +1,36 @@
+package altline.foodspo.buildsrc
+
+object Libs {
+    
+    object AndroidX {
+        const val coreKtx = "androidx.core:core-ktx:1.7.0"
+        
+        object Compose {
+            const val version = "1.1.1"
+            
+            const val ui = "androidx.compose.ui:ui:$version"
+            const val uiTooling = "androidx.compose.ui:ui-tooling:$version"
+            const val uiToolingPreview = "androidx.compose.ui:ui-tooling-preview:$version"
+            const val uiTestJunit4 = "androidx.compose.ui:ui-test-junit4:$version"
+            const val uiTestManifest = "androidx.compose.ui:ui-test-manifest:$version"
+            const val material = "androidx.compose.material:material:$version"
+        }
+        
+        object Activity {
+            const val activityCompose = "androidx.activity:activity-compose:1.4.0"
+        }
+        
+        object Lifecycle {
+            const val lifecycleRuntimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:2.4.1"
+        }
+        
+        object Test {
+            const val junit = "androidx.test.ext:junit:1.1.3"
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.4.0"
+        }
+    }
+    
+    object JUnit {
+        const val junit = "junit:junit:4.13.2"
+    }
+}
