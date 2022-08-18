dc1ef2bf1c1343b37f5ca7f54e4f4cc351798067
Dominik Zivko <domy.zivko@gmail.com>
2022-08-18 14:42:06 +0200

    Prevent view model recreation on every navigation

--- app/src/main/java/altline/foodspo/ui/core/navigation/NavGraph.kt
+++ app/src/main/java/altline/foodspo/ui/core/navigation/NavGraph.kt
@@ -1,7 +1,9 @@
 package altline.foodspo.ui.core.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -13,6 +15,11 @@ fun NavGraph(
     destinations: List<AppDestination>,
     startDestination: AppDestination
 ) {
+    // The single owner used for all destinations so that viewModels don't get recreated on every navigation
+    val viewModelStoreOwner = checkNotNull(LocalViewModelStoreOwner.current) {
+        "No ViewModelStoreOwner was provided for nav graph"
+    }
+
     NavHost(
         navController = navController,
         modifier = modifier,
@@ -21,9 +28,15 @@ fun NavGraph(
         for (dest in destinations) {
             composable(
                 route = dest.route,
-                arguments = dest.arguments,
-                content = dest.content
-            )
+                arguments = dest.arguments
+            ) {
+                CompositionLocalProvider(
+                    LocalViewModelStoreOwner provides viewModelStoreOwner
+                ) {
+                    dest.content(it)
+                }
+            }
         }
+
     }
 }
