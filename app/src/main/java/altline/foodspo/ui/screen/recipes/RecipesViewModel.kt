e456ee1c8edfceebd97fa441388e84a0a1338367
Dominik Živko <domy.zivko@gmail.com>
2022-09-15 18:07:46 +0200

    Implement missing add recipe to shopping list function on cards in recipes tab

--- app/src/main/java/altline/foodspo/ui/screen/recipes/RecipesViewModel.kt
+++ app/src/main/java/altline/foodspo/ui/screen/recipes/RecipesViewModel.kt
@@ -4,6 +4,7 @@ import altline.foodspo.data.RECIPE_PAGE_SIZE
 import altline.foodspo.data.core.paging.FlowPagingSource
 import altline.foodspo.data.core.paging.PagingAccessor
 import altline.foodspo.data.recipe.model.Recipe
+import altline.foodspo.domain.ingredient.AddRecipeToShoppingListUseCase
 import altline.foodspo.domain.recipe.GetMyRecipesUseCase
 import altline.foodspo.domain.recipe.GetSavedRecipesUseCase
 import altline.foodspo.ui.core.ViewModelBase
@@ -17,12 +18,14 @@ import androidx.paging.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class RecipesViewModel @Inject constructor(
     private val getMyRecipesUseCase: GetMyRecipesUseCase,
     private val getSavedRecipesUseCase: GetSavedRecipesUseCase,
+    private val addRecipeToShoppingListUseCase: AddRecipeToShoppingListUseCase,
     private val recipeUiMapper: RecipeUiMapper,
     savedStateHandle: SavedStateHandle
 ) : ViewModelBase<RecipesScreenUi>() {
@@ -73,7 +76,11 @@ class RecipesViewModel @Inject constructor(
     }
 
     private fun addIngredientsToShoppingList(recipeId: String) {
-        TODO("Not yet implemented")
+        viewModelScope.launch {
+            runAction {
+                addRecipeToShoppingListUseCase(recipeId)
+            }
+        }
     }
 
     private fun navigateToNewRecipe() {
